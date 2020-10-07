b7532b51049ba876165827573623870edc3d5e84
vijayaganesh <mvijayaganesh94@gmail.com>
2020-10-07 10:34:13 -0400

    Updated comments

--- src/depthquality/quality.py
+++ src/depthquality/quality.py
@@ -116,7 +116,7 @@ def compute_corner_coordinates(pointcloud, camera_matrix, corner_list):
 
 
 def fuzzy_match_corner(u, v, corners, pixel_tol=3):
-    # If the deprojected point is +/- 3 pixels away from the detected aruco corner
+    # If the deprojected point is +/- pixel_tol away from the detected aruco corner
     # consider it a matched point.
     for corner in corners:
         if corner[0] - pixel_tol <= v <= corner[0] + pixel_tol and \
